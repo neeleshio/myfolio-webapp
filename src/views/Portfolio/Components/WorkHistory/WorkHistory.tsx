db9a26ac8a21e715ab5b9e1a4f24409a39b44345
Neelesh Shetty <neelesh@pichainlabs.com>
2021-09-17 17:31:07 +0530

    added card flip

--- src/views/Portfolio/Components/WorkHistory/WorkHistory.tsx
+++ src/views/Portfolio/Components/WorkHistory/WorkHistory.tsx
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { companies } from './Companies'
+import CardFlip from 'react-card-flip';
 
 function WorkHistory() {
+    const [state, setState] = useState<any>({})
+
+    useEffect(() => {
+        let obj: any = {}
+
+        Object.values(companies).map((el, index) => {
+            obj[`card${index}`] = true
+        })
+
+        setState(obj)
+    }, [])
+
+    const handleCardFlip: Function = (index: string): void => {
+        setState((prevState: { [x: string]: any; }) => ({
+            ...prevState,
+            [index]: !prevState[index]
+        }))
+    }
+
     return (
         <div id="workHistory">
             <div className="container">
                 <h2>Work History (Recent 3)</h2>
-                <div className="card-container">
+                <div className="card-container front">
+                    {Object.values(companies).map((el, index: number) => (
+                        <>
+                            <CardFlip isFlipped={state?.[`card${index}`]} flipDirection="horizontal">
+                                <div key={index} className={`card card${index + 1}`} onClick={() => handleCardFlip(`card${index}`)}>
 
-                    {Object.values(companies).map((el, index) => (
-                        <div key={index} className={`card card${index + 1}`}>
-                            <div className="details-container">
-                                <h3>{el.designation.split(" ")[0]}</h3>
-                                <h3>{el.designation.split(" ")[1]}</h3>
-                                <h4>{el.timeline}</h4>
-                            </div>
-                            <div className="logo-container">
-                                <img alt={el.name} src={el.logo} />
-                                <h4>{el.name}</h4>
-                            </div>
-                        </div>
+                                    <h2>About Company</h2>
+                                    <h3>PiChain is a RegTech </h3>P
+                                    <h3>My Role</h3>
+                                </div>
+                                <div key={index} className={`card card${index + 1}`} onClick={() => handleCardFlip(`card${index}`)}>
+                                    <div className="details-container">
+                                        <h3>{el.designation.split(" ")[0]}</h3>
+                                        <h3>{el.designation.split(" ")[1]}</h3>
+                                        <h4>{el.timeline}</h4>
+                                    </div>
+                                    <div className="logo-container">
+                                        <img alt={el.name} src={el.logo} />
+                                        <h4>{el.name}</h4>
+                                    </div>
+                                </div>
+                            </CardFlip>
+                        </>
                     ))}
                 </div>
             </div>
