cc3ef148dc12d0939964499e09ee9d8794a21b96
Neelesh Shetty <neelesh@pichainlabs.com>
2021-10-14 00:40:45 +0530

    bookmark btn added

--- src/views/ViewArticle/Components/Leftbar/Leftbar.jsx
+++ src/views/ViewArticle/Components/Leftbar/Leftbar.jsx
@@ -1,23 +1,46 @@
 import React, { useState } from 'react'
 import { VscHeart } from 'react-icons/vsc'
-import { RiHeartFill } from 'react-icons/ri'
+import { RiHeartFill, RiShareForwardLine, RiBookmarkLine, RiBookmarkFill } from 'react-icons/ri'
 
 function Leftbar() {
     const [liked, setLiked] = useState(false)
+    const [saved, setSaved] = useState(false)
 
     const handleArticleLike = () => {
         setLiked(!liked)
     }
 
+    const handleArticleSave = () => {
+        setSaved(!saved)
+    }
+
     return (
         <div id="leftbar">
             <div className="leftbar_wrapper">
-                <i className={!liked && "heart_icon--disabled"} onClick={handleArticleLike}>
-                    <VscHeart />
-                </i>
-                <i className={liked && "heart_icon--disabled"} onClick={handleArticleLike}>
-                    <RiHeartFill className="filled" />
-                </i>
+                <div className="like_btn">
+                    <i className={!liked && "heart_icon"} onClick={handleArticleLike}>
+                        <VscHeart />
+                    </i>
+                    <i className={liked && "heart_icon"} onClick={handleArticleLike}>
+                        <RiHeartFill className="filled" />
+                    </i>
+                    <p>1,345 likes</p>
+                </div>
+                <div className="share_btn">
+                    <i>
+                        <RiShareForwardLine />
+                    </i>
+                    <p>Share</p>
+                </div>
+                <div className="bookmark_btn">
+                    <i className={!saved && "bookmark_icon"} onClick={handleArticleSave}>
+                        <RiBookmarkLine />
+                    </i>
+                    <i className={saved && "bookmark_icon"} onClick={handleArticleSave}>
+                        <RiBookmarkFill />
+                    </i>
+                    <p>Save</p>
+                </div>
             </div>
         </div>
     )
