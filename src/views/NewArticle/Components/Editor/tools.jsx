6e7ac2f481897e5c64381aa349eb0c1ae4ab946a
Neelesh Shetty <neelesh@pichainlabs.com>
2021-10-20 23:12:36 +0530

    editor js configs

--- src/views/NewArticle/Components/Editor/tools.jsx
+++ src/views/NewArticle/Components/Editor/tools.jsx
@@ -1,33 +1,39 @@
 import Embed from "@editorjs/embed";
-// import Table from "@editorjs/table";
 import List from "@editorjs/list";
-// import Warning from "@editorjs/warning";
 import Code from "@editorjs/code";
 import LinkTool from "@editorjs/link";
 import Image from "@editorjs/image";
-// import Raw from "@editorjs/raw";
 import Header from "@editorjs/header";
 import Quote from "@editorjs/quote";
-// import Marker from "@editorjs/marker";
-// import CheckList from "@editorjs/checklist";
-// import Delimiter from "@editorjs/delimiter";
 import InlineCode from "@editorjs/inline-code";
 import SimpleImage from "@editorjs/simple-image";
+// import Raw from "@editorjs/raw";
+// import Marker from "@editorjs/marker";
+// import CheckList from "@editorjs/checklist";
+// import Warning from "@editorjs/warning";
+// import Delimiter from "@editorjs/delimiter";
+// import Table from "@editorjs/table";
 
 export const EDITOR_JS_TOOLS = {
     embed: Embed,
-    // table: Table,
-    // marker: Marker,
     list: List,
-    // warning: Warning,
     code: Code,
-    // linkTool: LinkTool,
     image: Image,
-    // raw: Raw,
-    header: Header,
+    header: {
+        class: Header,
+        // inlineToolbar: ["link"],
+        config: {
+            placeholder: "Title"
+        }
+    },
     quote: Quote,
+    inlineCode: InlineCode,
+    simpleImage: SimpleImage,
+    linkTool: LinkTool,
     // checklist: CheckList,
     // delimiter: Delimiter,
-    inlineCode: InlineCode,
-    simpleImage: SimpleImage
+    // table: Table,
+    // marker: Marker,
+    // warning: Warning,
+    // raw: Raw,
 };
