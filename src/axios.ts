fe304194d5b334da4e21b17823d8cc50c26020fe
Neelesh Shetty <neeleshshetty01@gmail.com>
2021-11-05 23:01:56 +0530

    new baseurl

--- src/axios.ts
+++ src/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://2k3qde8fn5.execute-api.us-east-1.amazonaws.com/'
+    baseURL: 'https://j2ysckzfn2.execute-api.us-east-1.amazonaws.com'
 });
