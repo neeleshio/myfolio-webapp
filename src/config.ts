fc3487ba82bd0990ceeac7fe01d2634c0b849d87
Neelesh Shetty <neeleshshetty01@gmail.com>
2021-11-06 12:12:35 +0530

    change in process env

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-let BASE_URL = "http://localhost:3000"
+let BASE_URL = "https://j2ysckzfn2.execute-api.us-east-1.amazonaws.com"
 
-if (process.env.REACT_APP_ENV === "prod") {
-    BASE_URL = "https://j2ysckzfn2.execute-api.us-east-1.amazonaws.com"
+if (process.env.REACT_APP_ENV === "staging") {
+    BASE_URL = "http://localhost:3000"
 }
 
 export { BASE_URL }
