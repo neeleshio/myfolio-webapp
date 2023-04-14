ebc02c250e11bed39131d0f2d85ee7f4c825264f
neeleshio <neelesh@campk12.com>
2023-04-14 18:43:13 +0530

    TS children tyoe fix

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {ReactNode} from 'react'
 import { Topbar } from './Components'
 import Modal from '../../components/Modal'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
+
+interface Props {
+  children: ReactNode
+}
 
 const StyledMainLayout = styled.div`
     background-color: ${(props) => props.theme.layoutBg};
@@ -13,7 +17,7 @@ const StyledMainLayout = styled.div`
     }
 `
 
-const MainLayout = ({ children }) => {
+const MainLayout = ({ children }: Props) => {
     // const modalState = useSelector((state: any) => state.modal)
 
     return (
